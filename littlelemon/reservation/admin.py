6224a2fe9a02e2f173ce4508cec79391ecb78da8
maharajsandip <maharaj_sandip@berkeley.edu>
2025-04-21 20:50:05 -0700

    added menu and booking to reservation app

--- littlelemon/reservation/admin.py
+++ littlelemon/reservation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import menu
+from .models import booking
+
 
 # Register your models here.
+admin.site.register(menu)
+admin.site.register(booking)
--- littlelemon/reservation/models.py
+++ littlelemon/reservation/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=6)
+    booking_date = models.DateField()
+    
+class menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=10)
+    inventory = models.IntegerField(default=5)
